c34640de7978179453296f393e10285417f5ed3f
Michael Kalusche <michaelkalusche92@gmail.com>
2022-08-02 08:19:35 +0200

    changed from flexbox to grid, to handle the squares easier?

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
     const bodyContainer = document.getElementById("container");
-    let amountDiv = 256;
-
-    const changeColorBlack = () => {
-        addDiv.classList.add("blackBackground");
-    }
+    let amountDiv = 25;
 
     for (let i = 0; i < amountDiv; i++){
         const addDiv = document.createElement("div")
@@ -13,6 +9,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
         bodyContainer.append(addDiv)
     }
 
+    const squares = document.querySelectorAll('.squares')
+    squares.forEach(function(elem){
+        elem.addEventListener("mouseover", function(){
+            console.log("works bis hier")
+            elem.classList.add("blackBackground")
+        })
+    })
    
-
   });
+
+  function changeSize() {
+    let sizePrompt = prompt("Size?", "65")
+    amountDiv = sizePrompt;
+    console.log("helo melo")
+}
